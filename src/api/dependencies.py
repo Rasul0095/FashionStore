1939bfbe08c5fe235041a6cfdeea6c8d793e1d34
Rasul <dautov_rasul95@mail.ru>
2025-12-08 14:39:50 +0300

    added file dependencies.py: dependencies for async context manager and for user logic with jwt token

---
+++ src/api/dependencies.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+from fastapi import Depends, Request, HTTPException
+
+from src.utils.db_manager import DBManager
+from src.database import async_session_maker
+from src.services.auth import AuthService
+
+
+def get_token(request: Request) -> str:
+    token = request.cookies.get("access_token", None)
+    if not token:
+        raise HTTPException(status_code=401, detail="Вы не предоставили токен доступа")
+    return token
+
+def get_current_user_id(token: str = Depends(get_token)) -> int:
+    data = AuthService().decode_token(token)
+    return data["user_id"]
+
+async def get_db():
+    async with DBManager(session_factory=async_session_maker) as db:
+        yield db
+
+DBDep = Annotated[DBManager, Depends(get_db)]
+
